a6b944562f3dd8bd05e98d35733ec263dcb65378
Python1320 <python1320+gh@gmail.com>
2023-07-21 17:34:43 +0300

    Make sure initator is a player

--- lua/autorun/player_use.lua
+++ lua/autorun/player_use.lua
@@ -1,4 +1,5 @@
 local Tag = 'playerusehook'
+
 if SERVER then
 	util.AddNetworkString(Tag)
 	hook.Add("FindUseEntity", Tag, function(initator, target) end)
@@ -8,8 +9,8 @@ if SERVER then
 	})
 
 	hook.Add("PlayerUse", Tag, function(initator, target)
-		if not initator:KeyPressed(IN_USE) then return end
 		if not target:IsValid() or not target:IsPlayer() then return end
+		if not initator:IsPlayer() or not initator:KeyPressed(IN_USE) then return end
 		local nextt = ratelimit[target] or 0
 		local now = RealTime()
 		if now < nextt then return end
@@ -24,56 +25,44 @@ if SERVER then
 end
 
 -- helper
-
-local function getStartledBy(target,me)
+local function getStartledBy(target, me)
 	--TODO: Serverside
-	local startt=RealTime()
-	hook.Add("StartCommand",Tag,function(pl,mv)
+	local startt = RealTime()
+
+	hook.Add("StartCommand", Tag, function(pl, mv)
 		local now = RealTime()
-		local me=me or pl -- TODO
-		local cmd=mv
-		if now-startt>0.2 then 
-			hook.Remove("StartCommand",Tag)
+		local me = me or pl -- TODO
+		local cmd = mv
+
+		if now - startt > 0.2 then
+			hook.Remove("StartCommand", Tag)
+
 			return
 		end
-	
-		if --(SERVER and not me:IsBot()) or
-			 not me:Alive()
-			or me:GetMoveType() ~= MOVETYPE_WALK
-			or not me:IsOnGround()
-			then return end
-	
+
+		if not me:Alive() or me:GetMoveType() ~= MOVETYPE_WALK or not me:IsOnGround() then return end --(SERVER and not me:IsBot()) or
 		local mypos = me:GetPos()
-	
-		
-		local norm = target:GetPos()-mypos
+		local norm = target:GetPos() - mypos
 		norm.z = 0
 		norm:Normalize()
-	
-	
 		local va = cmd:GetViewAngles()
 		va.r = 0
 		va.p = 0
 		va.y = va.y
-	
-	
 		norm:Rotate(-va)
 		norm:Mul(250)
 		local fw, sd = cmd:GetForwardMove(), cmd:GetSideMove()
-	
 		fw = fw - norm.x
 		sd = sd + norm.y
 		--print(norm)
 		cmd:SetForwardMove(fw)
 		cmd:SetSideMove(sd)
-
-
 	end)
-	LookAtSmooth(target,1)
+
+	LookAtSmooth(target, 1)
 end
 
-
-local WHITE= Color(255, 255, 255)
+local WHITE = Color(255, 255, 255)
 local help = [[0 = disabled
  - 1 = look at user
  - 2 = look at user and play a sound from rp_react_to_use_saysound
@@ -91,7 +80,6 @@ local rp_react_to_use_only_afk = CreateClientConVar("rp_react_to_use_only_afk", 
 local rp_react_to_use_notification_every = CreateClientConVar("rp_react_to_use_notification_every", "3", true, false, "Only allow pressing every N seconds per player", 0, 1)
 local rp_react_to_use_notification_in_chat = CreateClientConVar("rp_react_to_use_notification_in_chat", "0", true, false, "0=notification 1=chat message", 0, 1)
 local rp_react_to_use_do_not_move = CreateClientConVar("rp_react_to_use_do_not_move", "0", true, false, "Only look towards player instead of moving away", 0, 1)
-
 local ratelimit = 0
 local ratelimit_msg = 0
 local msg_next_ok_ply = 0
@@ -140,8 +128,6 @@ local function PlayerUsedByPlayer(initator)
 				else
 					getStartledBy(initator)
 				end
-					
-					
 			end
 		elseif mode == 3 then
 			if now < ratelimit then return end
@@ -157,19 +143,21 @@ local function PlayerUsedByPlayer(initator)
 		if last_user ~= initator then
 			last_user = initator
 		elseif now < msg_next_ok_ply then
-			print("rate limit",initator)
+			print("rate limit", initator)
+
 			return
 		end
 
 		msg_next_ok_ply = now + math.Clamp(rp_react_to_use_notification_every, 0, 60 * 5)
 
 		if first then
-			if rp_react_to_use_notification_sound:Trim()~="" then
+			if rp_react_to_use_notification_sound:Trim() ~= "" then
 				surface.PlaySound(rp_react_to_use_notification_sound)
 			end
+
 			chat.AddText(Color(255, 222, 222), "[Notice] ", WHITE, "Disable the use messages by running in console: rp_react_to_use_notification 0")
 		elseif mode_msg >= 2 then
-			if rp_react_to_use_notification_sound:Trim()~="" then
+			if rp_react_to_use_notification_sound:Trim() ~= "" then
 				surface.PlaySound(rp_react_to_use_notification_sound)
 			end
 		end
