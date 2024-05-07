7b9e95f1662b5ab52f5bd20cfa9b546c02ca7fe1
Aoki <ger.vladislav2001@gmail.com>
2024-05-07 05:13:04 +0300

    Better UI + New features - Menu closes on ESC - Not draggable anymore - Allow moving while menu is open - Changed layout - More space for video title - Display owner - Open player sidebar - Volume bar - New media players spawns in the end of list - Removed players are now displayed red for 5 seconds before removing from list

--- lua/autorun/MediaPlayer_popuper.lua
+++ lua/autorun/MediaPlayer_popuper.lua
@@ -4,7 +4,7 @@ if !CLIENT then return end
 local FONT = 'Trebuchet24'
 local FONT_HEIGHT = draw.GetFontHeight( FONT )
 
-local function getMediaPlayers()
+function getMediaPlayers()
 	return ents.FindByClass( "mediaplayer_tv" )
 end
 
@@ -73,6 +73,10 @@ end
 
 local function titleGetter( player )
 	return function()
+		if !IsValid( player ) then
+			return 'Removed'
+		end
+
 		if player._mp == nil then
 			return 'Not subscribed'
 		end
@@ -164,7 +168,7 @@ function togglePopup( player )
 	end
 
 	function popup:Paint( w, h )
-		surface.SetDrawColor( 0, 0, 0, 210 )
+		surface.SetDrawColor( 0, 0, 0, 230 )
 		surface.DrawRect( 0, 0, w, h )
 
 		surface.SetDrawColor( 255, 255, 255 )
@@ -264,90 +268,166 @@ function togglePopup( player )
 	end
 end
 
-local function createMediaPlayerController( player, panel )
-	// Label
-	local label = vgui.Create( 'DLabel', panel )
-	label:Dock( FILL )
-	label:SetContentAlignment( 5 )
-	label:SetColor( color_white )
-
-	watch({
-		value = titleGetter( player ),
-		onChange = function( title )
-			label:SetText( title )
-		end,
-		callOnInit = true,
-		watchIf = function()
-			return IsValid( label )
-		end,
-	})
-
-	do // Subscribtion toggle
-		local subscribeButton = vgui.Create( 'DButton', panel )
-		subscribeButton:Dock( RIGHT )
-		subscribeButton:SetWide( 50 )
+local function createMediaPlayerController( player, panel, list )
+	do // Label
+		panel.title = vgui.Create( 'DLabel', panel )
+		panel.title:Dock( TOP )
+		panel.title:SetContentAlignment( 5 )
+		panel.title:SetColor( color_white )
 
 		watch({
-			value = function()
-				return player._mp != nil
-			end,
-			onChange = function( value )
-				if value then
-					subscribeButton:SetText( 'Stop' )
-				else
-					subscribeButton:SetText( 'Watch' )
-				end
+			value = titleGetter( player ),
+			onChange = function( title )
+				panel.title:SetText( title )
 			end,
 			callOnInit = true,
 			watchIf = function()
-				return IsValid( subscribeButton )
+				return IsValid( panel.title )
 			end,
-			callOnInit = true,
 		})
-
-		subscribeButton.DoClick = function()
-			togglePlayerSubscribtion( player )
-		end
 	end
 
-	do // Popup toggle
-		local popupButton = vgui.Create( 'DButton', panel )
-		popupButton:Dock( RIGHT )
-		popupButton:SetWide( 70 )
-		popupButton:DockMargin( 0, 0, 5, 0 )
+	do // Owner
+		panel.owner = vgui.Create( 'DLabel', panel )
+		panel.owner:Dock( TOP )
+		panel.owner:SetContentAlignment( 5 )
+		panel.owner:SetColor( color_white )
 
 		watch({
 			value = function()
-				return player._mp == nil and !IsValid( player.popup )
+				panel.owner.player = player.CPPIGetOwner and player:CPPIGetOwner() or panel.owner.player
+
+				return panel.owner.player and panel.owner.player:Name()
 			end,
-			onChange = function( disabled )
-				popupButton:SetDisabled( disabled )
+			onChange = function( name )
+				panel.owner:SetText( name and 'Owner: ' .. name or 'Unknown owner' )
 			end,
 			callOnInit = true,
 			watchIf = function()
-				return IsValid( popupButton )
+				return IsValid( panel.owner )
 			end,
 		})
+	end
 
-		watch({
-			value = function()
-				return IsValid( player.popup )
-			end,
-			onChange = function( value )
-				if value then
-					popupButton:SetText( 'Pop down' )
+	do // Controls
+		panel.controls = vgui.Create( 'DPanel', panel )
+		panel.controls:Dock( BOTTOM )
+		panel.controls:DockMargin( 0, 5, 0, 0 )
+		panel.controls:SetTall( 25 )
+
+		panel.controls.Paint = nil
+
+		do // Sidebar toggle
+			panel.sidebarButton = vgui.Create( 'DButton', panel.controls )
+			table.insert( panel.buttons, panel.sidebarButton )
+			panel.sidebarButton:Dock( LEFT )
+
+			function panel.sidebarButton:DoClick()
+				MediaPlayer.HideSidebar()
+
+				if list.showSideBarFor == player._mp then
+					list.showSideBarFor = nil
 				else
-					popupButton:SetText( 'Pop up' )
-				end
-			end,
-			callOnInit = true,
-			watchIf = function()
-				return IsValid( popupButton )
-			end,
-		})
+					timer.Simple( 0.1, function()
+						if !IsValid( player ) or !IsValid( player._mp ) or !IsValid( list ) then return end
 
-		popupButton.DoClick = function()
-			togglePopup( player )
+						MediaPlayer.ShowSidebar( player._mp )
+						list.showSideBarFor = player._mp
+					end )
+				end
+			end
+
+			watch({
+				value = function()
+					return list.showSideBarFor == player._mp
+				end,
+				onChange = function( isShowing )
+					if isShowing then
+						panel.sidebarButton:SetText( 'Hide sidebar' )
+						panel.sidebarButton:SetWide( 80 )
+					else
+						panel.sidebarButton:SetText( 'Show sidebar' )
+						panel.sidebarButton:SetWide( 85 )
+					end
+				end,
+				callOnInit = true,
+				watchIf = function()
+					return IsValid( panel.sidebarButton )
+				end,
+			})
+		end
+
+		do // Subscribtion toggle
+			panel.subscribeButton = vgui.Create( 'DButton', panel.controls )
+			table.insert( panel.buttons, panel.subscribeButton )
+			panel.subscribeButton:Dock( RIGHT )
+
+			watch({
+				value = function()
+					return player._mp != nil
+				end,
+				onChange = function( value )
+					if value then
+						panel.subscribeButton:SetText( 'Ubsub.' )
+						panel.subscribeButton:SetWide( 50 )
+					else
+						panel.subscribeButton:SetText( 'Watch' )
+						panel.subscribeButton:SetWide( 45 )
+					end
+				end,
+				callOnInit = true,
+				watchIf = function()
+					return IsValid( panel.subscribeButton )
+				end,
+				callOnInit = true,
+			})
+
+			function panel.subscribeButton:DoClick()
+				togglePlayerSubscribtion( player )
+			end
+		end
+
+		do // Popup toggle
+			panel.popupButton = vgui.Create( 'DButton', panel.controls )
+			table.insert( panel.buttons, panel.popupButton )
+			panel.popupButton:Dock( RIGHT )
+			panel.popupButton:DockMargin( 0, 0, 5, 0 )
+
+			watch({
+				value = function()
+					return player._mp == nil and !IsValid( player.popup )
+				end,
+				onChange = function( disabled )
+					panel.popupButton:SetDisabled( disabled )
+				end,
+				callOnInit = true,
+				watchIf = function()
+					return IsValid( panel.popupButton )
+				end,
+			})
+
+			watch({
+				value = function()
+					return IsValid( player.popup )
+				end,
+				onChange = function( value )
+					if value then
+						panel.popupButton:SetText( 'Pop down' )
+						panel.popupButton:SetWide( 69 )
+					else
+						panel.popupButton:SetText( 'Pop up' )
+						panel.popupButton:SetWide( 55 )
+					end
+				end,
+				callOnInit = true,
+				watchIf = function()
+					return IsValid( panel.popupButton )
+				end,
+			})
+
+			function panel.popupButton:DoClick()
+				togglePopup( player )
+			end
 		end
 	end
 end
@@ -363,86 +443,182 @@ mp_popuper = {
 			mp_popuper.close()
 		end
 
-		local frame = vgui.Create( 'DFrame' )
-		mp_popuper.frame = frame
-		frame:SetSize( 350, ScrH() )
-		frame:SetPos( ScrW() - frame:GetWide(), 0 )
-		frame:SetTitle( '' )
-		frame:MakePopup()
-		frame:SetSizable( true )
-		frame:ShowCloseButton( false )
-
-		frame:SetAlpha( 0 )
-		frame:AlphaTo( 255, 0.25 )
-
-		function frame:Close()
-			frame:SetMouseInputEnabled( false )
+		local frame do // Main frame
+			frame = vgui.Create( 'DFrame' )
+			mp_popuper.frame = frame
+			frame:DockPadding( 1, 29, 0, 0 )
+			frame:SetSize( 350, ScrH() )
+			frame:SetPos( ScrW() - frame:GetWide(), 0 )
+			frame:SetTitle( '' )
+			frame:MakePopup()
+			frame:SetSizable( true )
+			frame:SetDraggable( false )
+			frame:ShowCloseButton( false )
 			frame:SetKeyboardInputEnabled( false )
 
-			frame:AlphaTo( 0, 0.25, 0, function()
-				frame:Remove()
-				mp_popuper.close()
-			end )
-		end
+			frame:SetAlpha( 0 )
+			frame:AlphaTo( 255, 0.25 )
 
-		function frame:Paint( w, h )
-			surface.SetDrawColor( 0, 0, 0, 210 )
-			surface.DrawRect( 0, 0, w, h )
+			function frame:Close()
+				frame:SetMouseInputEnabled( false )
+				frame:SetKeyboardInputEnabled( false )
 
-			surface.SetDrawColor( 255, 255, 255 )
-			surface.DrawRect( 0, 0, 1, h )
-		end
+				MediaPlayer.HideSidebar()
 
-		do // Close button
-			local close = createCloseButton( frame, function()
-				mp_popuper.close()
-			end )
+				frame:AlphaTo( 0, 0.25, 0, function()
+					frame:Remove()
+					mp_popuper.close()
+				end )
+			end
 
-			close:SetPos( 2, 2 )
-		end
+			function frame:Paint( w, h )
+				surface.SetDrawColor( 0, 0, 0, 230 )
+				surface.DrawRect( 0, 0, w, h )
 
-		local list = vgui.Create( 'DScrollPanel', frame )
-		frame.list = list
-		list:Dock( FILL )
+				surface.SetDrawColor( 255, 255, 255 )
+				surface.DrawRect( 0, 0, 1, h )
+			end
 
-		watch({
-			value = function()
-				return getMediaPlayers()
-			end,
-			deepCheck = true,
-			onChange = function()
-				mp_popuper.refresh()
-			end,
-			watchIf = function()
-				return IsValid( frame )
-			end,
-			callOnInit = true,
-		})
-	end,
-	refresh = function()
-		if !IsValid( mp_popuper.frame ) then
-			return
-		end
-
-		local list = mp_popuper.frame.list
-		list:Clear()
-
-		for _, player in ipairs( getMediaPlayers() ) do
-			local panel = vgui.Create( 'DPanel', list )
-			panel:SetTall( 50 )
-			panel:Dock( TOP )
-			panel:DockMargin( 0, 0, 0, 5 )
-			panel:DockPadding( 5, 5, 5, 5 )
-
-			function panel:Paint( w, h )
-				if self:IsHovered() then
-					surface.SetDrawColor( 255, 255, 255 )
-					surface.DrawRect( 0, 0, 1, h )
-					surface.DrawRect( w - 1, 0, 1, h )
+			function frame:Think()
+				if gui.IsGameUIVisible() then
+					gui.HideGameUI()
+					mp_popuper.close()
+					frame.Think = nil
 				end
 			end
 
-			createMediaPlayerController( player, panel )
+		end
+
+		do // Close button
+			frame.close = createCloseButton( frame, function()
+				mp_popuper.close()
+			end )
+
+			frame.close:SetPos( 2, 2 )
+		end
+
+		do // TV list
+			frame.list = vgui.Create( 'DScrollPanel', frame )
+			frame.list:Dock( FILL )
+			frame.list:DockMargin( 5, 0, 0, 0 )
+			frame.list.mediaPlayers = {}
+
+			function frame.list:Refresh()
+				for _, player in ipairs( getMediaPlayers() ) do
+					local panel = frame.list.mediaPlayers[player]
+					if IsValid( panel ) then continue end
+
+					panel = vgui.Create( 'DPanel', frame.list )
+					frame.list.mediaPlayers[player] = panel
+					panel:SetTall( 75 )
+					panel:Dock( TOP )
+					panel:DockMargin( 0, 0, 5, 5 )
+					panel:DockPadding( 5, 5, 5, 5 )
+					panel.buttons = {}
+
+					function panel:Think()
+						if !IsValid( player ) then
+							for _, button in ipairs( panel.buttons ) do
+								button:SetDisabled( true )
+							end
+
+							timer.Simple( 5, function()
+								if IsValid( panel ) then
+									panel:Remove()
+									frame.list.mediaPlayers[player] = nil
+								end
+							end )
+						end
+					end
+
+					function panel:Paint( w, h )
+						if IsValid( player ) then
+							surface.SetDrawColor( 150, 150, 150 )
+							surface.DrawOutlinedRect( 0, 0, w, h )
+						else
+							surface.SetDrawColor( 255, 0, 0, 230 )
+							surface.DrawRect( 0, 0, w, h )
+						end
+
+
+						if self:IsHovered() then
+							surface.SetDrawColor( 255, 255, 255 )
+							surface.DrawOutlinedRect( 0, 0, w, h )
+						end
+					end
+
+					createMediaPlayerController( player, panel, frame.list )
+				end
+			end
+
+			function frame.list:Adapt()
+			end
+
+			watch({
+				value = function()
+					return getMediaPlayers()
+				end,
+				deepCheck = true,
+				onChange = function()
+					frame.list:Refresh()
+				end,
+				watchIf = function()
+					return IsValid( frame )
+				end,
+				callOnInit = true,
+			})
+		end
+
+		do // Volume slider
+			frame.footerFrame = vgui.Create( 'DPanel', frame )
+			frame.footerFrame:Dock( BOTTOM )
+			local inlinePadding = 10
+			local blockPadding = 5
+
+			function frame.footerFrame:Adapt()
+				local height = 0
+
+				for _, child in ipairs( self:GetChildren() ) do
+					child:SetPos( inlinePadding, height + blockPadding )
+					child:SetWide( frame:GetWide() - inlinePadding * 2 )
+					height = height + child:GetTall()
+				end
+
+				self:SetTall( height + blockPadding * 2 )
+			end
+
+			function frame.footerFrame:Paint( w, h )
+				surface.SetDrawColor( 255, 255, 255 )
+				surface.DrawRect( 0, 0, w, 1 )
+			end
+
+			do // Volume slider
+				frame.volume = vgui.Create( 'DNumSlider', frame.footerFrame )
+				frame.volume:SetText( 'Volume' )
+				frame.volume:SetMin( 0 )
+				frame.volume:SetMax( 100 )
+				frame.volume:SetDecimals( 0 )
+				frame.volume:SetValue( MediaPlayer.Volume() * 100 )
+
+				function frame.volume:OnValueChanged( value )
+					MediaPlayer.Volume( value / 100 )
+				end
+
+				local interval = 8
+				function frame.volume.Slider:Paint( w, h )
+					local ticks = math.round( ( w - 8 ) / interval )
+					local step = ( w - 8 ) / ticks
+
+					surface.SetDrawColor( 210, 210, 210 )
+					surface.DrawRect( 7, h / 2, w - 14, 1 )
+
+					for i = 1, ticks do
+						surface.DrawRect( math.floor( i * step ), h / 2 + 4, 1, 5 )
+					end
+				end
+			end
+
+			frame.footerFrame:Adapt()
 		end
 	end,
 	close = function()
