02c73907eb3ae3ae1e6fe6d7e6edc68e5af0eb15
PotcFdk <PotcFdk@gmx.net>
2024-12-17 14:39:36 +0100

    Simplify the achievement example because the original example was missing half the networking logic and it might be confusing without context.

--- lua/metaworks/metachievements/server/modules/example.lua
+++ lua/metaworks/metachievements/server/modules/example.lua
@@ -1,24 +1,15 @@
--- taken from: https://gitlab.com/metastruct/internal/MetaWorks-metastruct/-/blob/master/lua/metaworks/metachievements/server/modules/bunnyhop.lua?ref_type=heads
 do return end -- remove this line
 
 if not MetAchievements then return end
 
 local Tag = "MetAchievements"
-local id = "bunnyhop"
+local id = "chat"
 
 MetAchievements.RegisterAchievement(id, {
-	title = "Breakneck Pace",
-	description = "Break the sound barrier by bunnyhopping."
+	title = "Chat Person",
+	description = "Say something."
 })
 
-util.AddNetworkString(Tag.."_"..id)
-
-net.Receive(Tag.."_"..id, function(len, ply)
-	local speed = ply:GetVelocity():Length()
-	if speed >= 5000 and not ply:InVehicle() then
-		MetAchievements.UnlockAchievement(ply, id) -- we're done here.
-	else -- re-arm clientside
-		net.Start(Tag.."_"..id)
-		net.Send(ply)
-	end
+hook.Add("PlayerSay", Tag.."_"..id, function(ply)
+	MetAchievements.UnlockAchievement(ply, id)
 end)
