c005f621921c3782cfd84b164585d4ee58897492
Brianna <utsuho@riseup.net>
2025-03-21 11:43:46 +0000

    feat: dmg mode check and use easylua for targeting

--- lua/aowl/commands/eatfriends.lua
+++ lua/aowl/commands/eatfriends.lua
@@ -3,13 +3,20 @@
 
 if CLIENT then
 	CreateClientConVar("eating_disallow_command", 0, true, true)
-	CreateClientConVar("eating_friends_only_command", 0, true, true)
+	CreateClientConVar("eating_friends_only_command", 1, true, true)
 	return
 end
 
 if SERVER then
 	if aowl then
 		local maxRange = 196 ^ 2
+		local dmgType = {
+            [1] = true, -- God Mode
+            [2] = true, -- No Player Damage
+            [4] = true, -- Buddha Mode
+            [5] = true, -- Godded players can't hurt you
+            [6] = true, -- Shop NPC mode
+        }
 		local function CanEatPlayer(caller, targetPlayer)
 			if not IsValid(targetPlayer) or not targetPlayer:IsPlayer() then
 				return false, "That is not a player."
@@ -24,17 +31,21 @@ if SERVER then
 					return false, "The player has disabled being eaten."
 				end
 
-				local friendOnly = targetPlayer:GetInfoNum("eating_friends_only_command", 0) == 1
+				local friendOnly = targetPlayer:GetInfoNum("eating_friends_only_command", 1) == 1
 				if friendOnly and friendsh.GetFriendStatus(caller:UserID(), targetPlayer:UserID()) ~= "friends" then
 					return false, "The player only allows friends to eat them."
 				end
-			end
 
+                local dmgMode = targetPlayer:GetInfoNum("cl_dmg_mode", 0)
+                if targetPlayer:HasGodMode() or dmgType[dmgMode] then
+                    return false, "The player is in a protected state and cannot be eaten."
+                end
+			end
 			return true
 		end
 
 		aowl.AddCommand("eat", "Eat your friends!", function(caller, line, target)
-			local targetPlayer = aowl.FindPlayer(target)
+			local targetPlayer = easylua.FindEntity(target)
 			if not IsValid(targetPlayer) then
 				return false, "Player not found."
 			end
