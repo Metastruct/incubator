a8f4edf8f7c78ad87374c176367e673cab6c40d1
Python1320 <python1320+gh@gmail.com>
2026-06-07 02:41:39 +0300

    Hack to enable HTML input
    also some usability

--- lua/autorun/client/wsdupe_browser.lua
+++ lua/autorun/client/wsdupe_browser.lua
@@ -29,6 +29,8 @@ local function init()
                 container:SetSize(1, 1)
 
                 local initialized = false
+                local lastPaintTime = 0
+                local focusHeld = false
 
                 local function Init()
                         if initialized then return end
@@ -40,23 +42,45 @@ local function init()
 
                         local btn = vgui.Create("DButton", actbar)
                         btn:Dock(LEFT)
-                        btn:SetText("#dupes.arm")
-                        btn:SetEnabled(false)
+                        btn:SetText("#vgui_close")
+                        btn:SetEnabled(true)
                         btn:SetWidth(130)
-                        btn:SetImage("icon16/arrow_down.png")
-                        btn.DoClick = function()
-                                if not WSID then return end
-                                btn:SetEnabled(false)
-                                DownloadAndArm(WSID)
-                                timer.Simple(1, function()
-                                        if IsValid(btn) then btn:SetEnabled(true) end
-                                end)
+                        btn:SetImage("icon16/stop.png")
+
+                        local oldBtnPaint = btn.Paint
+                        btn.Paint = function(self, w, h)
+                                if oldBtnPaint then oldBtnPaint(self, w, h) end
+                                if WSID then
+                                        local pulse = 0.5 + 0.5 * math.sin(RealTime() * 4)
+                                        surface.SetDrawColor(100 + 155 * pulse, 180 + 75 * pulse, 100 + 80 * pulse, 120)
+                                        surface.DrawRect(0, 0, w, h)
+                                else
+                                        surface.SetDrawColor(220, 160, 160, 120)
+                                        surface.DrawRect(0, 0, w, h)
+                                end
                         end
+
+                        btn.DoClick = function()
+                                if g_SpawnMenu and g_SpawnMenu.Close then g_SpawnMenu:Close() end
+                                if WSID then
+                                        btn:SetEnabled(false)
+                                        DownloadAndArm(WSID)
+                                        timer.Simple(1, function()
+                                                if IsValid(btn) then btn:SetEnabled(true) end
+                                        end)
+                                end
+                        end
+
+                        local searchLbl = vgui.Create("DLabel", actbar)
+                        searchLbl:Dock(LEFT)
+                        searchLbl:SetText("#searchbar_placeholder")
+                        searchLbl:SetTextColor(Color(180, 180, 180))
+                        searchLbl:DockMargin(4, 0, 4, 0)
+                        searchLbl:SizeToContents()
 
                         local search = vgui.Create("DTextEntry", actbar)
                         search:Dock(LEFT)
                         search:SetWide(180)
-                        search:SetText("metastruct")
                         search:SetPlaceholderText("Search workshop...")
                         search:SetUpdateOnType(false)
                         search.OnEnter = function(self)
@@ -79,23 +103,18 @@ local function init()
                         HTML:Dock(FILL)
                         nav:SetHTML(HTML)
 
-                        HTML.OnMousePressed = function(self, key)
-                                if key == MOUSE_LEFT then
-                                        self:RequestFocus()
-                                end
-                        end
-
                         HTML.OnFinishLoadingDocument = function(_, url)
                                 local id = ExtractWSID(url)
                                 if id then
                                         WSID = tonumber(id)
                                         btn:SetEnabled(true)
-                                        btn:SetText(language.GetPhrase("dupes.arm") .. " #" .. WSID)
+                                        btn:SetText("Deploy")
+                                        btn:SetImage("icon16/page_paste.png")
                                         lbl:SetText("Ready - dupe #" .. WSID)
                                 else
                                         WSID = nil
-                                        btn:SetEnabled(false)
-                                        btn:SetText("#dupes.arm")
+                                        btn:SetText("Close")
+                                        btn:SetImage("icon16/stop.png")
                                         lbl:SetText("Navigate to a workshop item page")
                                 end
                         end
@@ -109,7 +128,23 @@ local function init()
                 local OldPaint = container.Paint
                 container.Paint = function(self, w, h)
                         Init()
-                        if OldPaint then return OldPaint(self, w, h) end
+                        if OldPaint then OldPaint(self, w, h) end
+
+                        local t = RealTime()
+                        if lastPaintTime == 0 or t - lastPaintTime > 0.5 then
+                                print("wsdupe: OnTextEntryGetFocus called, focusHeld=true")
+                                hook.Run("OnTextEntryGetFocus", HTML)
+                                focusHeld = true
+                        end
+                        lastPaintTime = t
+                end
+
+                container.Think = function(self)
+                        if focusHeld and RealTime() - lastPaintTime > 0.2 then
+                                print("wsdupe: KillFocus called")
+                                focusHeld = false
+                                if HTML and HTML.KillFocus then HTML:KillFocus() end
+                        end
                 end
 
                 return container
